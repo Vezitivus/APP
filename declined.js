0e20fc580415afd711f30b42f34fb273a54b9c2f
Vezitivus <vezitivus@gmail.com>
2025-02-05 17:35:34 +0200

    Update declined.js

--- declined.js
+++ declined.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
     container.innerHTML = `
         <h1 class="title">Žēl, ka šoreiz nepievienosies</h1>
         <p>Bet ja pārdomāsi, reģistrācija vienmēr pieejama.</p>
-        <video id="byeVideo" src="bye.MOV" autoplay playsinline loop></video>
+        <video id="byeVideo" src="bye.mov" autoplay playsinline loop></video>
     `;
 
     const video = document.getElementById("byeVideo");
