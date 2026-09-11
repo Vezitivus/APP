f9e0bc00a60b6000c8d77646666ac739623d0e82
Vezitivus <vezitivus@gmail.com>
2026-09-11 14:49:38 +0300

    Add Build Radar offline cache

---
+++ riga-build-radar/sw.js
@@ -0,0 +1,5 @@
+const CACHE='buildradar-v1';
+const CORE=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
